df6582058b688e8dd0db7ebd5fd7b071c27262f9
Anne-Marie Nault <anne.ma@gmail.com>
2021-09-11 10:50:10 -0400

    improved css, added bookmark form on show page, changed rating for float

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @bookmark = Bookmark.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:create]
   end
   resources :bookmarks, only: [:destroy]
 end
