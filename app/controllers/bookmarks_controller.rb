09d3490e7c5efc8189f5a9c352e17500593e7d73
Anne-Marie Nault <anne.ma@gmail.com>
2021-09-11 16:23:01 -0400

    fixed redirect after deletion

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,7 +12,9 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
     @bookmark.destroy
+    redirect_to list_path(@list)
   end
 
   private
